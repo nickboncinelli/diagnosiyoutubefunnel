b7e8037936bc0eee2aff58907c7c9c3bf91b023a
Nicolò Boncinelli <nicoloboncinelli@192.168.1.8>
2026-04-22 18:59:44 +0700

    Add GTM tracking (GTM-TBK762J9)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter, Syne } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+
+const GTM_ID = "GTM-TBK762J9";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -49,7 +52,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it" className={`${inter.variable} ${syne.variable} h-full antialiased`}>
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col" style={{ backgroundColor: "#050505", color: "#e6e6e6" }}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
